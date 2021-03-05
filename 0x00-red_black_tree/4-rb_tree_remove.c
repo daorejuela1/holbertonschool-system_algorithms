efe067f33e54f7795c2332a261b7b8591680e7c1
daorejuela1 <daorejuela1@outlook.com>
2021-03-05 01:44:00 +0000

    added remove node from BTS trial

--- 0x00-red_black_tree/4-rb_tree_remove.c
+++ 0x00-red_black_tree/4-rb_tree_remove.c
@@ -1,1 +1,54 @@
-#!/usr/bin/env bash
+#include "rb_trees.h"
+
+/**
+ * min_value - gets the min value of a BST
+ * @node: node to be search of
+ *
+ * Return: min value of the node
+ */
+rb_tree_t *min_value(rb_tree_t *node)
+{
+	rb_tree_t *current = node;
+
+	while (current && current->left != NULL)
+		current = current->left;
+	return (current);
+}
+
+/**
+ * rb_tree_remove - removes the red black tree indicated value
+ * @root: root of the red black tree
+ * @n: value to remove
+ *
+ * Return: Root of the tree
+ */
+rb_tree_t *rb_tree_remove(rb_tree_t *root, int n)
+{
+	rb_tree_t *temp = NULL;
+
+	if (root == NULL)
+		return (root);
+	if (n < root->n)
+		root->left = rb_tree_remove(root->left, n);
+	else if (n > root->n)
+		root->right = rb_tree_remove(root->right, n);
+	else
+	{
+		if (root->left == NULL)
+		{
+			temp = root->right;
+			free(root);
+			return (temp);
+		}
+		else if (root->right == NULL)
+		{
+			temp = root->left;
+			free(root);
+			return (temp);
+		}
+		temp = minValueNode(root->right);
+		root->n = temp->n;
+		root->right = rb_tree_remove(root->right, temp->n);
+	}
+	return (root);
+}
