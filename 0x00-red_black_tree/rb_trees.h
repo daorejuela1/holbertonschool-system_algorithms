ab590f00169848d941738d9def8af1ea43e9b501
daorejuela1 <daorejuela1@outlook.com>
2021-03-05 06:31:23 +0000

    added prototype for task 4

--- 0x00-red_black_tree/rb_trees.h
+++ 0x00-red_black_tree/rb_trees.h
@@ -44,4 +44,5 @@ void rb_tree_print(const rb_tree_t *tree);
 int rb_tree_is_valid(const rb_tree_t *tree);
 rb_tree_t *rb_tree_insert(rb_tree_t **tree, int value);
 rb_tree_t *array_to_rb_tree(int *array, size_t size);
+rb_tree_t *rb_tree_remove(rb_tree_t *root, int n);
 #endif
