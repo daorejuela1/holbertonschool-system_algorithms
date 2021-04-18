4dd87819be46fce89c249a4692d16247c681334f
daorejuela1 <daorejuela1@outlook.com>
2021-04-18 20:47:05 +0000

    added task 3

--- 0x01-graphs/3-graph_delete.c
+++ 0x01-graphs/3-graph_delete.c
@@ -1,1 +1,31 @@
-#!/usr/bin/env bash
+#include "graphs.h"
+
+/**
+ * graph_delete - Frees a graph in memory
+ * @graph: pointer to graph
+ * Returns: Nothing
+ */
+void graph_delete(graph_t *graph)
+{
+	vertex_t *vertex, *vertex_temp;
+	edge_t *edge, *edge_temp;
+
+	if (!graph)
+		return;
+	vertex = graph->vertices;
+	while (vertex)
+	{
+		free(vertex->content);
+		edge = vertex->edges;
+		while (edge)
+		{
+			edge_temp = edge;
+			edge = edge->next;
+			free(edge_temp);
+		}
+		vertex_temp = vertex;
+		vertex = vertex->next;
+		free(vertex_temp);
+	}
+	free(graph);
+}
