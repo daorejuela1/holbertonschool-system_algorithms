267092abac1d96bf87ad1c5cd078b3e3d468bd93
daorejuela1 <daorejuela1@outlook.com>
2021-04-18 05:21:35 +0000

    Fixed task 1

--- 0x01-graphs/1-graph_add_vertex.c
+++ 0x01-graphs/1-graph_add_vertex.c
@@ -1,37 +1,49 @@
 #include "graphs.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * graph_add_vertex - adds a vertex to an existing graph
- * @graph: pointer to the graph
- * @str: string value for new vertex
- * Return: pointer to the created vertex, or NULL on failure
- */
-vertex_t *graph_add_vertex(graph_t *graph, const char *str)
+ * init_vertex - Initialize a vertex object
+ * @str: Vertext content
+ * @index: vertex index
+ * Return: pointer to vertex or null in error
+ **/
+static vertex_t *init_vertex(const char *str, size_t index)
 {
-	vertex_t *new_vertex, *vertex;
+	vertex_t *new_vertex = NULL;
 
-	if (!graph || !str)
-		return (NULL);
-	vertex = graph->vertices;
-	while (vertex && vertex->next)
-	{
-		if (!strcmp(vertex->content, str))
-			return (NULL);
-		vertex = vertex->next;
-	}
 	new_vertex = malloc(sizeof(vertex_t));
 	if (!new_vertex)
 		return (NULL);
 	new_vertex->content = strdup(str);
-	if (!new_vertex->content)
-	{
-		free(new_vertex);
-		return (NULL);
-	}
-	new_vertex->index = graph->nb_vertices++;
-	if (vertex)
-		vertex->next = new_vertex;
-	else
-		graph->vertices = new_vertex;
+	new_vertex->nb_edges = 0;
+	new_vertex->edges = NULL;
+	new_vertex->next = NULL;
+	new_vertex->index = index;
 	return (new_vertex);
+}
+
+/**
+ * graph_add_vertex - adds a vertex to an existing graph
+ * @graph: pointer to graph
+ * @str: content of vertex
+ * Return: pointer to the created vertex, or NULL on failure
+ **/
+vertex_t *graph_add_vertex(graph_t *graph, const char *str)
+{
+	vertex_t **created_vertex;
+
+	if (!graph || !str)
+		return (NULL);
+	created_vertex = &graph->vertices;
+	while (*created_vertex != NULL)
+	{
+		if (strcmp(str, (*created_vertex)->content) == 0)
+			return (NULL);
+		created_vertex = &(*created_vertex)->next;
+	}
+
+	*created_vertex = init_vertex(str, graph->nb_vertices);
+	graph->nb_vertices += 1;
+	return (*created_vertex);
 }
