13a96437f4aef39e1a1d33f3bd9c2cb8caada924
daorejuela1 <daorejuela1@outlook.com>
2021-05-07 04:42:53 +0000

    Updated required files

--- 0x02-huffman_coding/huffman.h
+++ 0x02-huffman_coding/huffman.h
@@ -1,7 +1,8 @@
-#ifndef HUFFMAN_H
-#define HUFFMAN_H
+#ifndef _HUFFMAN_H_
+#define _HUFFMAN_H_
 
 #include "heap/heap.h"
+#include <stdio.h>
 
 /**
  * struct symbol_s - Stores a char and its associated frequency
@@ -15,14 +16,37 @@ typedef struct symbol_s
 	size_t freq;
 } symbol_t;
 
-symbol_t *symbol_create(char data, size_t freq);
+/**
+ * struct char_freq_s - Stores a two vector arrays for symbols and
+ * their frequencies, plus the size of each
+ *
+ * @data_v: vector of characters
+ * @freq_v: vector of frequencies
+ * @size: size of the vectors
+ */
+typedef struct char_freq_s
+{
+	char *data;
+	size_t *freq;
+	size_t size;
+} char_freq_t;
 
-int data_cmp(void *node_a, void *node_b);
-void free_data(void *data);
+/* Prototypes */
+symbol_t *symbol_create(char data, size_t freq);
 heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 int huffman_extract_and_insert(heap_t *priority_queue);
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size);
-int huffman_codes(char *data, size_t *freq, size_t size);
-void print_huffman_codes(binary_tree_node_t *tree, unsigned long path);
 
-#endif
+/* Huffman Codes Prototypes */
+int huffman_codes(char *data, size_t *freq, size_t size);
+void print_huffman_tree_r(binary_tree_node_t *root, char *code, size_t depth);
+size_t get_depth(binary_tree_node_t *root, size_t depth);
+void free_codes(binary_tree_node_t *root);
+
+
+/* Main Huffman Program Prototypes */
+int compress(const char *in_file, const char *out_file);
+int decompress(const char *in_file, const char *out_file);
+char_freq_t *count_symbols(const char *in_file);
+
+#endif /* _HUFFMAN_H_ */
