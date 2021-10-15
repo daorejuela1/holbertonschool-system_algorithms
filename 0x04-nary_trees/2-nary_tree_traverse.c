87ad86050c8ddd2d187c26bc5312bc87039d77c7
daorejuela1 <daorejuela1@outlook.com>
2021-10-15 01:19:09 +0000

    Added traversal order

--- 0x04-nary_trees/2-nary_tree_traverse.c
+++ 0x04-nary_trees/2-nary_tree_traverse.c
@@ -1,1 +1,43 @@
-#!/usr/bin/env bash
+#include "nary_trees.h"
+
+
+/**
+ * biggest_depth - goes through an N-ary tree, node by node.
+ * @root: root node of the tree to traverse
+ * @action: pointer to a function to execute for each node being traversed.
+ * @depth: counter of the current depth
+ * Return:  return the biggest depth of the tree pointed to by root
+ */
+static size_t biggest_depth(nary_tree_t const *root,
+		void (*action)(nary_tree_t const *node, size_t depth), size_t depth)
+{
+	nary_tree_t *child = NULL;
+	unsigned int big_depth, current_depth;
+
+	action(root, depth);
+	child = root->children;
+	big_depth = depth;
+	while (child)
+	{
+		current_depth = biggest_depth(child, action, depth + 1);
+		if (current_depth > big_depth)
+			big_depth = depth;
+		child = child->next;
+	}
+	return (big_depth);
+}
+/**
+ * nary_tree_traverse - goes through an N-ary tree, node by node.
+ * @root: root node of the tree to traverse
+ * @action: pointer to a function to execute for each node being traversed.
+ *
+ * Return:  return the biggest depth of the tree pointed to by root
+ */
+size_t nary_tree_traverse(nary_tree_t const *root,
+		void (*action)(nary_tree_t const *node, size_t depth))
+{
+
+	if (!root)
+		return (0);
+	return (biggest_depth(root, action, 0));
+}
