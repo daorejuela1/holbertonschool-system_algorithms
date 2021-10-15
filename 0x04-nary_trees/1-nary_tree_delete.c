066817af3900122cd9ab70d32cc6a2d0e8a24eb3
daorejuela1 <daorejuela1@outlook.com>
2021-10-15 00:58:58 +0000

    Added free logic

--- 0x04-nary_trees/1-nary_tree_delete.c
+++ 0x04-nary_trees/1-nary_tree_delete.c
@@ -1,1 +1,26 @@
-#!/usr/bin/env bash
+#include "nary_trees.h"
+
+/**
+ * nary_tree_delete - deallocates n-ary tree
+ * @tree: root of n-ary tree
+ *
+ * Return: Nothing
+ */
+void nary_tree_delete(nary_tree_t *tree)
+{
+	nary_tree_t *tmp_child = NULL, *tmp_node = NULL;
+
+	if (!tree)
+		return;
+	tmp_child = tree->children;
+	while (tmp_child)
+	{
+		nary_tree_delete(tmp_child->children);
+		free(tmp_child->content);
+		tmp_node = tmp_child->next;
+		free(tmp_child);
+		tmp_child = tmp_node;
+	}
+	free(tree->content);
+	free(tree);
+}
